6116748c44fd6d07a9698b3fa759d1f7a33d3dd9
GokulPavan99 <121012564+GokulPavan99@users.noreply.github.com>
2024-01-12 20:06:45 +0530

    Create main.tf

---
+++ Terraform_statefile_storing_in_s3bucket/main.tf
@@ -0,0 +1,80 @@
+# terraform state file : It contains the information of infrastructure that we created the aws resources by using the terraform
+
+  provider "aws" {
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""
+    backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "name should be unique"
+    key            = "global/s3/terraform.tfstate"    # path in the s3 bucket
+    region         = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+
+}
+
+resource "aws_instance" "amazon-server" {
+  ami           = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "demo-server"
+  }
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "name should be unique"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
